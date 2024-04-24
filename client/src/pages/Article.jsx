4d7e5ffe9663fe46977ee9e05d8b89a07126cf72
ArchiePerera <archivaldoperera@gmail.com>
2024-04-24 13:48:48 +0200

    CLIENT: Article skeletton

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -4,9 +4,35 @@ import { useState, useEffect } from "react"
 
 const Article = () => {
 
+    const { id } = useParams()
+    const [article, setArticle] = useState("")
+
+    useEffect(() => {
+
+        const fetchOneArticle = async () => {
+            
+            try {
+
+                const res = await axios.get(`/api/articles/${ id }`)
+
+                setArticle(res.data)
+
+            }
+            catch (e) {
+
+                console.log(e)
+
+            }
+        }
+
+        fetchOneArticle()
+
+
+    }, [])
+
     return(
         <>
-        <h1>Test</h1>
+        <h1>{article.title}</h1>
         <NavLink to={"/"}>Home</NavLink>
         </>
     )

a4e4e0278c397c37f249789a423199abd01a64c1
ArchiePerera <archivaldoperera@gmail.com>
2024-06-11 17:16:36 +0200

    404 page

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const ErrorPage = () => {
     return (
-        <h1>ErrorPage</h1>
+        <section className="error">
+            <NavLink className="btn primary" to="/">Home</NavLink>
+            <h1>404</h1>
+            <h2>Page introuvable</h2>
+        </section>
     )
 }
 
